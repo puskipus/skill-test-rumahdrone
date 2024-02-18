2b82fffdd50179947b2304b8a6a3ced8587105b9
Thoriq Achmad <thoriqachmad.r@gmail.com>
2024-02-18 16:11:13 +0700

    first init and login

---
+++ backend-rumah-drone/database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'staff1',
+            'email' => 'staff1@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'staff',
+        ]);
+    }
+}
---
+++ frontend-rumah-drone/src/components/LogoBrand/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "./rumah_drone_logo.jpg";
+
+export default function LogoBrand() {
+  return (
+    <img className="mr-2" width={"200px"} src={logo} alt="logo rumah drone" />
+  );
+}
